cacdc51d1ccbcfee1d63e70ad8df22660ab15a90
Thomas Salemy <tps220@lehigh.edu>
2017-11-23 18:54:15 -0500

    algorithms commit

---
+++ algorithms/primality/prime_sieve/primeSieve.hpp
@@ -0,0 +1,4 @@
+//Thomas Salemy
+//Prime Sieve Class
+#include <cmath>
+bool* primeSieve(unsigned int size);
---
+++ algorithms/sorting/HeapSort/HeapSort.hpp
@@ -0,0 +1,6 @@
+//Thomas Salemy
+//Header File
+
+void heap_sort(int*, unsigned int);
+void maxheap_sort(int*, unsigned int);
+void minheap_sort(int*, unsigned int);
