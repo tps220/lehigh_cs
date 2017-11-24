268e5fb6bc1a661faa92559c0386a91f59c9aa7b
Louis Salemy <louissalemy@Louis-Salemys-iMac.local>
2017-11-24 11:30:07 -0500

    quick sort added

---
+++ algorithms/sorting/QuickSort/QuickSort.hpp
@@ -0,0 +1,9 @@
+//Thomas Salemy
+//Quick Sort Header File
+
+void quicksort(int *array, unsigned int size);
+
+
+void swap(int &a, int &b);
+void qsort(int *array, int l, int r);
+int partition(int* array, int l, int r);
